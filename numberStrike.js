6bb0864389751185d80c96364c50b599f625e9fa
hotcream3904 <dkssud6757@naver.com>
2023-06-13 16:07:02 +0900

    정답숫자생성

--- numberStrike.js
+++ numberStrike.js
@@ -1,16 +1,29 @@
-function userInput(){
-    let userInputNum = document.getElementById('user-input-num').value;
-    let inputNumArray = userInputNum.split('');
-    console.log(inputNumArray);
-    
-    
+function userInput() {
+  let userInputNum = document.getElementById('user-input-num').value;
+  let inputNumArray = userInputNum.split('');
+  console.log(inputNumArray);
+}
+
+function answerNum() {
+  let answerFirstNum = Math.floor(Math.random() * 10);
+  let answerSecondNum = Math.floor(Math.random() * 10);
+  while (answerFirstNum === answerSecondNum) {
+    answerSecondNum = Math.floor(Math.random() * 10);
+  }
+  let answerThirdNum = Math.floor(Math.random() * 10);
+  while (answerFirstNum === answerThirdNum || answerSecondNum === answerThirdNum) {
+    answerThirdNum = Math.floor(Math.random() * 10);
+  }
+  console.log(answerFirstNum);
+  console.log(answerSecondNum);
+  console.log(answerThirdNum);
+  let answerArray = [answerFirstNum, answerSecondNum, answerThirdNum]
+  console.log(answerArray);
+  return;
+}
+answerNum();
 
 
 
-    }
 
-    
-    
-    function answerNum(){
-      
-    }
+
