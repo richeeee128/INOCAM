cde04339a9f603270de06acd8d58ba1616602410
hotcream3904 <dkssud6757@naver.com>
2023-06-13 16:52:57 +0900

    여기서마무리를해야해..

--- numberStrike.js
+++ numberStrike.js
@@ -1,29 +1,32 @@
 function userInput() {
+  let a = 0;
   let userInputNum = document.getElementById('user-input-num').value;
   let inputNumArray = userInputNum.split('');
-  console.log(inputNumArray);
+  return inputNumArray;
+};
+
+
+let a = userInput();
+console.log(a);
+
+let answerFirstNum = Math.floor(Math.random() * 10);
+let answerSecondNum = Math.floor(Math.random() * 10);
+while (answerFirstNum === answerSecondNum) {
+  answerSecondNum = Math.floor(Math.random() * 10);
 }
+let answerThirdNum = Math.floor(Math.random() * 10);
+while (
+  answerFirstNum === answerThirdNum ||
+  answerSecondNum === answerThirdNum
+) {
+  answerThirdNum = Math.floor(Math.random() * 10);
+}
+console.log(`${answerFirstNum}${answerSecondNum}${answerThirdNum}`);
 
-  let answerFirstNum = Math.floor(Math.random() * 10);
-  let answerSecondNum = Math.floor(Math.random() * 10);
-  while (answerFirstNum === answerSecondNum) {
-    answerSecondNum = Math.floor(Math.random() * 10);
-  }
-  let answerThirdNum = Math.floor(Math.random() * 10);
-  while (answerFirstNum === answerThirdNum || answerSecondNum === answerThirdNum) {
-    answerThirdNum = Math.floor(Math.random() * 10);
-  }
-  console.log(`${answerFirstNum}${answerSecondNum}${answerThirdNum}`)
-
-
-  
 //볼의 경우의 수
 let ballNum = 0;
 
 //스트라이크의 경우의수
 let StrikeNum = 0;
 
-
-
 console.log(`${ballNum}B${StrikeNum}S`);
-
