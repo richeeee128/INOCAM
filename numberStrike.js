9d2e1fa5b15ab99121a51ddf7ba12b78426ae1b3
hotcream3904 <dkssud6757@naver.com>
2023-06-13 14:38:12 +0900

    test

---
+++ numberStrike.js
@@ -0,0 +1,16 @@
+function userInput(){
+    let userInputNum = document.getElementById('user-input-num').value;
+    let inputNumArray = userInputNum.split('');
+    console.log(inputNumArray);
+    
+    
+
+
+
+    }
+
+    
+    
+    function answerNum(){
+      
+    }
