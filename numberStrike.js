95ebb60244be3714f7cef95273b8d88afc3ee2bb
hotcream3904 <dkssud6757@naver.com>
2023-06-13 16:33:06 +0900

    js:함수를없앴다

--- numberStrike.js
+++ numberStrike.js
@@ -4,7 +4,6 @@ function userInput() {
   console.log(inputNumArray);
 }
 
-function answerNum() {
   let answerFirstNum = Math.floor(Math.random() * 10);
   let answerSecondNum = Math.floor(Math.random() * 10);
   while (answerFirstNum === answerSecondNum) {
@@ -14,16 +13,17 @@ function answerNum() {
   while (answerFirstNum === answerThirdNum || answerSecondNum === answerThirdNum) {
     answerThirdNum = Math.floor(Math.random() * 10);
   }
-  console.log(answerFirstNum);
-  console.log(answerSecondNum);
-  console.log(answerThirdNum);
-  let answerArray = [answerFirstNum, answerSecondNum, answerThirdNum]
-  console.log(answerArray);
-  return;
-}
-answerNum();
+  console.log(`${answerFirstNum}${answerSecondNum}${answerThirdNum}`)
+
+
+  
+//볼의 경우의 수
+let ballNum = 0;
+
+//스트라이크의 경우의수
+let StrikeNum = 0;
 
 
 
-
+console.log(`${ballNum}B${StrikeNum}S`);
 
