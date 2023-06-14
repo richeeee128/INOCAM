4982cf4c272b16bbafef8680f2993ab09b078120
hotcream3904 <dkssud6757@naver.com>
2023-06-14 21:40:53 +0900

    동블의 정답만드는함수 리팩터링

--- numberStrike.js
+++ numberStrike.js
@@ -1,27 +1,45 @@
 //정답만드는 함수
-let answer = '정답숫자담는변수';
-let answerFirstNum = Math.floor(Math.random() * 10);
-let answerSecondNum = Math.floor(Math.random() * 10);
-while (answerFirstNum === answerSecondNum) {
-  answerSecondNum = Math.floor(Math.random() * 10);
-}
-let answerThirdNum = Math.floor(Math.random() * 10);
-while (
-  answerFirstNum === answerThirdNum ||
-  answerSecondNum === answerThirdNum
-) {
-  answerThirdNum = Math.floor(Math.random() * 10);
+const answer = [];
+const answerFirstNum = createRandomNum();
+let answerSecondNum = createRandomNum();
+let answerThirdNum = createRandomNum();
+
+while(isDuplicateNum(answerFirstNum, answerSecondNum, answerThirdNum)) {
+  answerSecondNum = createRandomNum();
+  answerThirdNum = createRandomNum();
 }
 
-answer = [`${answerFirstNum}`, `${answerSecondNum}`, `${answerThirdNum}`];
+answer.push(answerFirstNum, answerSecondNum, answerThirdNum);
+
+function createRandomNum() {
+  return Math.floor(Math.random() * 10);
+}
+
+function isDuplicateNum(answerFirstNum, answerSecondNum, answerThirdNum) {
+  if (answerFirstNum === answerSecondNum) {
+    return true;
+  }
+
+  if (
+    answerFirstNum === answerThirdNum ||
+    answerSecondNum === answerThirdNum
+  ) {
+    return true;
+  }
+
+   return false;
+
+}
+
+console.log(answer);
 
 //하하 ^^ 너는 전역변수란다.
 let counter = 0;
 
 //사용자가 정답을 입력하고 그 입력받은 것을 배열로 담는 로직
 function gameStart() {
-  let userInputNum = document.getElementById('user-input-num').value;
-  let inputNumArray = userInputNum.split('');
+  const userInputNum = document.getElementById('user-input-num').value;
+  const inputNumArray = userInputNum.split('');
 
   //볼의 경우의 수
   let ballNum = 0;
@@ -32,7 +50,7 @@ function gameStart() {
       }
     }
   }
- 
+
   //스트라이크의 경우의수 - 배열매서드(forEach)를 사용
   let StrikeNum = 0;
 
@@ -44,15 +62,12 @@ function gameStart() {
 
   ballNum = ballNum - StrikeNum;
 
-  console.log(counter);
-  counter ++;
+  counter++;
 
+  if (StrikeNum === 3) {
+    alert(`${counter}번째 시도! 정답을 맞췄습니다! 게임이 종료됩니다`);
+  }
+  let hintMessage = document.getElementById('hint');
 
-  if(StrikeNum === 3){
-      alert(`${counter}번째 시도! 정답을 맞췄습니다! 게임이 종료됩니다`);
-    }
-    let hintMessage = document.getElementById('hint')
-    
-    hintMessage.innerHTML= `${ballNum}B${StrikeNum}S`;
-    
+  hintMessage.innerHTML = `${ballNum}B${StrikeNum}S`;
 }
