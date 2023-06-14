660d9f0fbc5d6bb2f9c8d2e3b994298d45d3c80b
hotcream3904 <dkssud6757@naver.com>
2023-06-14 14:30:05 +0900

    볼을 구현하였으나 스트라이크였다

--- numberStrike.js
+++ numberStrike.js
@@ -16,9 +16,6 @@ while (
 answer = [`${answerFirstNum}`, `${answerSecondNum}`, `${answerThirdNum}`];
 
 console.log(answer);
-console.log(answer[0]);
-console.log(answer[1]);
-console.log(answer[2]);
 
 //사용자가 정답을 입력하고 그 입력받은 것을 배열로 담는 로직
 function gameStart() {
@@ -26,33 +23,31 @@ function gameStart() {
   let inputNumArray = userInputNum.split('');
   console.log(inputNumArray);
 
-  //볼의 경우의 수
+  //볼의 경우의 수 - 배열매서드를 사용
   let ballNum = 0;
-
-  //   for (let i = 0; i < 3; i++) {
-  //     if (inputNumArray.includes(answer[i])) {
-  //       ballNum++;
-  //       console.log(ballNum);
-  //     }
-  // }
-  //     console.log(inputNumArray.includes(answer[1]));
-
   inputNumArray.forEach((item, index) => {
     if (item.includes(answer[index])) {
       ballNum++;
     }
   });
 
+  
+
   console.log(ballNum);
 
   //스트라이크의 경우의수
   let StrikeNum = 0;
-  if (
-    inputNumArray[0] === answer[0] ||
-    inputNumArray[1] === answer[1] ||
-    inputNumArray[2] === answer[2]
-  ) {
-    StrikeNum ++;
+  for (let i = 0; i < 3; i++) {
+    if (
+      inputNumArray[0] === answer[0] ||
+      inputNumArray[1] === answer[1] ||
+      inputNumArray[2] === answer[2]
+    ) {
+      StrikeNum++;
+    }
   }
+
+  console.log(StrikeNum);
+
   console.log(`${ballNum}B${StrikeNum}S`);
 }
